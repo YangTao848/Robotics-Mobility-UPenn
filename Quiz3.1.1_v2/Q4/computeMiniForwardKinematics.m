88c56a0f5550c05574e16b9aa46129f39547b774
YangTao848 <31799659+YangTao848@users.noreply.github.com>
2017-12-01 19:27:08 +0800

    Update computeMiniForwardKinematics.m
    my right solution for this problem!

--- Quiz3.1.1_v2/Q4/computeMiniForwardKinematics.m
+++ Quiz3.1.1_v2/Q4/computeMiniForwardKinematics.m
@@ -1,8 +1,19 @@
 function [endeff] = computeMiniForwardKinematics(rads1,rads2)
 
-beta = (rads1 - rads2)/2;
-alpha = (1/2) * (rads1 + rads2) + pi;
-r = cos(pi - beta) + sqrt(4 - sin(pi - beta) * sin(pi - beta));
-[x,y] = pol2cart(alpha,r);
-endeff = [x,y];
-end
+alpha = pi + (rads1 + rads2) / 2;
+
+beta = rads1 - rads2;
+
+L_longer = 2.0;
+
+L_shorter = 1.0;
+
+syms x;
+S = solve(cos(pi-beta/2) == (x^2 + L_shorter^2 - L_longer^2) / (2 * L_shorter * x));
+
+L = double(S);
+
+X = L * cos(alpha);
+
+Y = L * sin(alpha);
+endeff = [X, Y];
