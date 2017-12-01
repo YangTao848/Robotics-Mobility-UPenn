70358e48cfdfa0a2e93b169bf79d57cd3babadaa
YangTao848 <31799659+YangTao848@users.noreply.github.com>
2017-12-01 19:25:22 +0800

    Update computeRrInverseKinematics.m
    right solution!

--- Quiz3.1.1_v2/Q3/computeRrInverseKinematics.m
+++ Quiz3.1.1_v2/Q3/computeRrInverseKinematics.m
@@ -2,12 +2,9 @@ function [rads1,rads2] = computeRrInverseKinematics(X,Y)
 
 syms theta1 theta2 ;
 
-%S = vpasolve([cos(theta1+theta2)+cos(theta1) == X, sin(theta1+theta2)+sin(theta1) == Y],[theta1,theta2],[0 2*pi;0 2*pi])
-%rads1 = double(S.theta1);
-%rads2 = double(S.theta2);
-eqn1 = cos(theta1+theta2)+cos(theta1) == X;
-eqn2 = sin(theta1+theta2)+sin(theta1) == Y;
-eqns = [eqn1,eqn2];
-S = solve(eqns)
-rads1 = double(S.theta1(1,1))
-rads2 = double(S.theta2(1,1))
+
+S = solve([theta1>0,theta1<pi/2,theta2>0,theta2<2*pi],[cos(theta1)+cos(theta1+theta2)==X,sin(theta1)+sin(theta1+theta2)==Y],[theta1,theta2]);
+
+rads1 = double(S.theta1);
+
+rads2 = double(S.theta2);
